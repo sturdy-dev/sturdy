3695484585712fcafdb44d9a7b1b0c925312ea62
Gustav Westling <gustav@getsturdy.com>
2022-01-18 16:28:16 +0000

    web: new footer to fit with the new documentation sites
    --- sturdy ---
    user_id: 847dfd0c-49bf-40c5-8870-74a12fca0d60
    view_id: 128badfe-5c58-487e-adfd-c4f1e4fd160f
    workspace_id: 3a27c759-de29-4abd-abcd-e55a56e8893b

---
+++ web/src/layouts/PublicOpenSource.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="">
+    <ClientOnly>
+      <IndexNavbar :user="user" />
+    </ClientOnly>
+
+    <slot></slot>
+    <DocumentationFooter />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import IndexNavbar from '../components/IndexNavbar.vue'
+import IndexFooter from '../components/IndexFooter.vue'
+import { ClientOnly } from 'vite-ssr/vue'
+import DocumentationFooter from "../organisms/DocumentationFooter.vue";
+
+export default defineComponent({
+  components: {
+    DocumentationFooter,
+    IndexNavbar,
+    ClientOnly,
+  },
+  setup() {
+    return {
+      user: null,
+    }
+  },
+})
+</script>
