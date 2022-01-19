fcf4a5439b853d3854219cbfb2c06607079a4342
Kiril Videlov <kiril@videlov.com>
2022-01-19 14:07:56 +0000

    web/docs: importing code from github
    --- sturdy ---
    user_id: 2be622cc-03ce-41fc-a2fa-13daff3d1d53
    view_id: 6745e2f5-71b4-4d2e-bf12-8e08a630dba6
    workspace_id: f949e0cb-5e63-44a5-bf66-20cbb3f88980

---
+++ web/src/pages/docs/HowToImportCodeFromGitHub.vue
@@ -0,0 +1,71 @@
+<template>
+  <PublicLeftSidebar>
+    <template #sidebar>
+      <DocsSidebar />
+    </template>
+    <template #default>
+      <div class="prose p-4 max-w-[800px]">
+        <h1 id="how-sturdy-augments-git">How to import code from GitHub</h1>
+
+        <p>
+          If you have an existing repository on GitHub, adding it to Sturdy is easy.
+          With a few clicks you can give access to one or multiple specific repositories.
+          This integration is not just for getting the code but also for the bridge interaction between Sturdy and GitHub.
+        </p>
+
+        <ol>
+          <li>Click on the three dots icon in the top left corner and select "New codebase"</li>
+          <li>On the codebase creation screen click the "Import repositories" button within the "Coming from GitHub?"
+            panel in the bottom.</li>
+          <li>On GitHub, select the organization which contains the codebase you want to import</li>
+          <li>Follow the GitHub prompt for authentication</li>
+          <li>Select one or multiple repositories you wish to work with</li>
+          <li>You will be redirected back to Sturdy</li>
+          <li>When the repositories are ready within Sturdy, you will be notified - this is usually instant</li>
+          <li>To edit your code, follow the "How to: edit code" guide</li>
+          <!-- TODO: Link -->
+        </ol>
+
+        <p>
+          Once you have the repository in a Sturdy codebase, all of the latest code as well as the history will be available within Sturdy.
+          The README.md file is nicely rendered and you can browse files and folders, just as you are used to.
+        </p>
+
+        <p>It is important to note that whenever new code is pushed to the default (main/master) branch on GitHub, by
+          your or somebody else, Sturdy will keep itself up to date automatically.</p>
+
+        <DocsInfoBox>
+          The newly created Sturdy codebase is also aware of your existing pull request, which will show up as workspaces.
+        </DocsInfoBox>
+
+        <p>
+          <em>Coming soon:</em> Pull Requests created by others on your team will also show up as workspaces so that you can get the code with one click.
+          This will work even if others on the team are not using Sturdy.
+        </p>
+      </div>
+    </template>
+  </PublicLeftSidebar>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import PublicLeftSidebar from '../../layouts/PublicLeftSidebar.vue'
+import { useHead } from '@vueuse/head'
+import DocsSidebar from '../../organisms/docs/DocsSidebar.vue'
+import DocsInfoBox from '../../molecules/DocsInfoBox.vue'
+
+export default defineComponent({
+  components: { DocsSidebar, PublicLeftSidebar, DocsInfoBox },
+  setup() {
+    // TODO: Remove when we're launching!
+    useHead({
+      meta: [
+        {
+          name: 'robots',
+          content: 'noindex',
+        },
+      ],
+    })
+  },
+})
+</script>
