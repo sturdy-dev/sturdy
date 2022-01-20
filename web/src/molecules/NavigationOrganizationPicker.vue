9dd68a40e23b314fc44616ffbc052c4518933d64
Gustav Westling <gustav@getsturdy.com>
2022-01-20 13:29:59 +0000

    web: create codebases in a organization
    --- sturdy ---
    user_id: 847dfd0c-49bf-40c5-8870-74a12fca0d60
    view_id: 128badfe-5c58-487e-adfd-c4f1e4fd160f
    workspace_id: e6dcb3e3-cff6-4f2f-b9e6-6c8bbe1dbe70

---
+++ web/src/molecules/NavigationOrganizationPicker.vue
@@ -0,0 +1,105 @@
+<template>
+  <Listbox v-if="val" v-model="val" as="div">
+    <div class="mt-1 relative">
+      <ListboxButton
+        class="bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+      >
+        <span class="block truncate">{{ val.name }}</span>
+        <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+          <SelectorIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+        </span>
+      </ListboxButton>
+
+      <transition
+        leave-active-class="transition ease-in duration-100"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
+      >
+        <ListboxOptions
+          class="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
+        >
+          <ListboxOption
+            v-for="organization in organizations"
+            :key="organization.id"
+            v-slot="{ active, selected }"
+            as="template"
+            :value="organization"
+          >
+            <li
+              :class="[
+                active ? 'text-white bg-blue-600' : 'text-gray-900',
+                'cursor-default select-none relative py-2 pl-3 pr-9',
+              ]"
+            >
+              <span :class="[selected ? 'font-semibold' : 'font-normal', 'block truncate']">
+                {{ organization.name }}
+              </span>
+
+              <span
+                v-if="selected"
+                :class="[
+                  active ? 'text-white' : 'text-blue-600',
+                  'absolute inset-y-0 right-0 flex items-center pr-4',
+                ]"
+              >
+                <CheckIcon class="h-5 w-5" aria-hidden="true" />
+              </span>
+            </li>
+          </ListboxOption>
+        </ListboxOptions>
+      </transition>
+    </div>
+  </Listbox>
+</template>
+
+<script lang="ts">
+import { ref, PropType, defineComponent, toRef } from 'vue'
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/vue'
+import { CheckIcon, SelectorIcon } from '@heroicons/vue/solid'
+import { gql } from '@urql/vue'
+import { OrganizationPickerFragment } from './__generated__/NavigationOrganizationPicker'
+
+const ORGANIZATION_FRAGMENT = gql`
+  fragment OrganizationPicker on Organization {
+    id
+    name
+  }
+`
+
+export default defineComponent({
+  components: {
+    Listbox,
+    ListboxButton,
+    ListboxOption,
+    ListboxOptions,
+    CheckIcon,
+    SelectorIcon,
+  },
+  props: {
+    organizations: {
+      type: Object as PropType<Array<OrganizationPickerFragment>>,
+      required: true,
+    },
+    modelValue: {
+      type: Object as PropType<OrganizationPickerFragment>,
+      required: true,
+    },
+  },
+  emits: ['update:modelValue'],
+  data() {
+    return { val: null }
+  },
+  watch: {
+    modelValue(val) {
+      // <- copy this
+      this.val = val
+    },
+    val(val) {
+      this.$emit('update:modelValue', val)
+    },
+  },
+  mounted() {
+    this.val = this.modelValue
+  },
+})
+</script>
--- web/src/pages/organization/OrganizationSettingsPage.vue
+++ web/src/pages/organization/OrganizationSettingsPage.vue
@@ -20,10 +20,12 @@
 </template>
 
 <script lang="ts">
-import PaddedApp from '../../layouts/PaddedApp.vue'
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
 import { gql, useQuery } from '@urql/vue'
-import { OrganizationQuery, OrganizationQueryVariables } from './__generated__/View'
+import {
+  OrganizationSettingsPageQuery,
+  OrganizationSettingsPageQueryVariables,
+} from './__generated__/View'
 import { useRoute } from 'vue-router'
 import OrganizationMembers from '../../organisms/organization/OrganizationMembers.vue'
 import Header from '../../molecules/Header.vue'
@@ -34,12 +36,11 @@ export default defineComponent({
   components: { PaddedAppLeftSidebar, OrganizationMembers, Header, VerticalNavigation },
   setup() {
     let route = useRoute()
-    let orgID = route.params.id as string
 
-    let { data } = useQuery<OrganizationQuery, OrganizationQueryVariables>({
+    let { data } = useQuery<OrganizationSettingsPageQuery, OrganizationSettingsPageQueryVariables>({
       query: gql`
-        query Organization($id: ID!) {
-          organization(id: $id) {
+        query OrganizationSettingsPage($shortID: ID!) {
+          organization(shortID: $shortID) {
             id
             name
             members {
@@ -58,7 +59,7 @@ export default defineComponent({
       `,
       requestPolicy: 'cache-and-network',
       variables: {
-        id: orgID,
+        shortID: route.params.organizationSlug,
       },
     })
 
