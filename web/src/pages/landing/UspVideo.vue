1f5d55734ca9a107d3d6b15cfc52c777ca64d90c
Kiril Videlov <kiril@videlov.com>
2022-01-25 16:26:02 +0000

    web/landing: First stab at index page update
    --- sturdy ---
    user_id: 2be622cc-03ce-41fc-a2fa-13daff3d1d53
    view_id: 6745e2f5-71b4-4d2e-bf12-8e08a630dba6
    workspace_id: f949e0cb-5e63-44a5-bf66-20cbb3f88980

---
+++ web/src/pages/landing/UspVideo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <video
+      autoplay
+      loop
+      muted
+      playsinline
+      alt="Placeholder"
+      class="rounded-lg object-cover object-center"
+    >
+      <source src="../../assets/Landing/WorkspaceSwitching.webm" type="video/webm" />
+      <source src="../../assets/Landing/WorkspaceSwitching.mp4" type="video/mp4" />
+    </video>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Video',
+}
+</script>
+
+<style scoped></style>
