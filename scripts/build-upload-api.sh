73f3a1b32d06706e59321b7b934f4511c3dcaf48
Nikita Galaiko <nikita@galaiko.rocks>
2022-01-25 08:50:17 +0000

    scripts: use single dockerfile
    --- sturdy ---
    user_id: cc33c18b-27e5-470d-8589-f9391ca37590
    view_id: 2c064d23-2410-4e86-939f-91c56dae2fae
    workspace_id: f8fa0c2a-e6ff-4c03-9c23-bc914a63a8f7

--- scripts/build-upload-api.sh
+++ scripts/build-upload-api.sh
@@ -2,17 +2,21 @@
 
 set -euo pipefail
 
+CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Authenticate the docker daemon to the registry
 aws ecr get-login-password --region eu-north-1 |
   docker login --username AWS --password-stdin 902160009014.dkr.ecr.eu-north-1.amazonaws.com
 
 VERSION=$(date +%Y-%m-%d-%H-%M-%S)
-ECR_NAME="902160009014.dkr.ecr.eu-north-1.amazonaws.com/mutagen-ssh:${VERSION}"
+ECR_NAME="902160009014.dkr.ecr.eu-north-1.amazonaws.com/api:${VERSION}"
 
 docker buildx build \
   --platform linux/amd64 \
   --tag "$ECR_NAME" \
+  --build-arg API_BUILD=cloud \
+  --target api \
   --push \
-  .
+  "$CWD/.."
 
 echo "Successfully built and pushed $ECR_NAME :-)"
