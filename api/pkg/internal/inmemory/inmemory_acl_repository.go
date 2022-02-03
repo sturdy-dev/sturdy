3b8f20904723595fceb2717383c4f0cfe3e1f010
Gustav Westling <gustav@getsturdy.com>
2022-02-03 17:35:00 +0000

    api/pkg/mutagen: add test for listing allowed files
    --- sturdy ---
    user_id: 847dfd0c-49bf-40c5-8870-74a12fca0d60
    view_id: 128badfe-5c58-487e-adfd-c4f1e4fd160f
    workspace_id: 157faa85-697d-473b-b69c-b7bade5288d0

---
+++ api/pkg/internal/inmemory/inmemory_acl_repository.go
@@ -0,0 +1,43 @@
+package inmemory
+
+import (
+	"context"
+	"database/sql"
+
+	"getsturdy.com/api/pkg/codebase/acl"
+	db_acl "getsturdy.com/api/pkg/codebase/acl/db"
+)
+
+type inMemoryAclRepo struct {
+	acls []acl.ACL
+}
+
+func NewInMemoryAclRepo() db_acl.ACLRepository {
+	return &inMemoryAclRepo{
+		acls: make([]acl.ACL, 0),
+	}
+}
+
+func (r *inMemoryAclRepo) Create(_ context.Context, a acl.ACL) error {
+	r.acls = append(r.acls, a)
+	return nil
+
+}
+
+func (r *inMemoryAclRepo) Update(_ context.Context, a acl.ACL) error {
+	for k, v := range r.acls {
+		if v.ID == a.ID {
+			r.acls[k] = a
+		}
+	}
+	return nil
+}
+
+func (r *inMemoryAclRepo) GetByCodebaseID(_ context.Context, codebaseID string) (acl.ACL, error) {
+	for _, v := range r.acls {
+		if v.CodebaseID == codebaseID {
+			return v, nil
+		}
+	}
+	return acl.ACL{}, sql.ErrNoRows
+}
