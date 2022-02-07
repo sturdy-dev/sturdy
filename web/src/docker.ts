00dc46dfdc14f97a7f3d5ea71847c57a9fad162b
Kiril <kiril@videlov.com>
2022-02-07 10:23:23 +0000

    web/oneliner: update oneliner
    --- sturdy ---
    user_id: 2be622cc-03ce-41fc-a2fa-13daff3d1d53
    workspace_id: 31f0439b-88d3-45f4-a2ae-6b685dd928a5

--- web/src/docker.ts
+++ web/src/docker.ts
@@ -1,5 +1,5 @@
 export const DOCKER_ONELINER = `docker run --interactive --tty \\
-    --pull \\
+    --pull always \\
     --publish 30080:80 --publish 30022:22 \\
     --volume "$HOME/.sturdydata:/var/data" \\
     getsturdy/server`
