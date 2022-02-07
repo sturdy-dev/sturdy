b5314de0cc1a12d8a511f118fc0cc9d29f0e4fb5
Nikita Galaiko <nikita@galaiko.rocks>
2022-02-07 08:50:39 +0000

    app: add eslint
    --- sturdy ---
    user_id: cc33c18b-27e5-470d-8589-f9391ca37590
    view_id: 2c064d23-2410-4e86-939f-91c56dae2fae
    workspace_id: f8fa0c2a-e6ff-4c03-9c23-bc914a63a8f7

---
+++ app/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+}
