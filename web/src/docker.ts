cad0f338190ea428a90f2409bc72211633dc362b
Nikita Galaiko <nikita@galaiko.rocks>
2022-02-07 14:38:59 +0000

    web/docs: docker oneliner must not use —tty to make ctrl-c work
    --- sturdy ---
    user_id: cc33c18b-27e5-470d-8589-f9391ca37590
    view_id: 2c064d23-2410-4e86-939f-91c56dae2fae
    workspace_id: f8fa0c2a-e6ff-4c03-9c23-bc914a63a8f7

--- web/src/docker.ts
+++ web/src/docker.ts
@@ -1,4 +1,4 @@
-export const DOCKER_ONELINER = `docker run --interactive --tty \\
+export const DOCKER_ONELINER = `docker run --interactive \\
     --pull always \\
     --publish 30080:80 --publish 30022:22 \\
     --volume "$HOME/.sturdydata:/var/data" \\
