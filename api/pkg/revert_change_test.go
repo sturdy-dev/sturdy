f8c345c56d981a6e5d4c4e8a4bdf339db49e641b
Gustav Westling <gustav@getsturdy.com>
2022-02-16 13:43:15 +0000

    api/pkg: use dependency injection in TestRevertChangeFromSnapshot and TestRevertChangeFromView
    --- sturdy ---
    user_id: 847dfd0c-49bf-40c5-8870-74a12fca0d60
    view_id: 128badfe-5c58-487e-adfd-c4f1e4fd160f
    workspace_id: ac83b73f-5568-44bc-af8b-db64c50f006d

--- api/pkg/revert_change_test.go
+++ api/pkg/revert_change_test.go
@@ -8,60 +8,29 @@ import (
 	"testing"
 	"time"
 
-	"getsturdy.com/api/pkg/analytics/disabled"
+	"go.uber.org/dig"
+
+	"getsturdy.com/api/pkg/analytics"
 	"getsturdy.com/api/pkg/auth"
-	service_auth "getsturdy.com/api/pkg/auth/service"
-	db_change "getsturdy.com/api/pkg/change/db"
-	"getsturdy.com/api/pkg/change/decorate"
-	graphql_change "getsturdy.com/api/pkg/change/graphql"
-	service_change "getsturdy.com/api/pkg/change/service"
-	workers_ci "getsturdy.com/api/pkg/ci/workers"
 	"getsturdy.com/api/pkg/codebase"
-	db_acl "getsturdy.com/api/pkg/codebase/acl/db"
-	provider_acl "getsturdy.com/api/pkg/codebase/acl/provider"
 	db_codebase "getsturdy.com/api/pkg/codebase/db"
-	graphql_codebase "getsturdy.com/api/pkg/codebase/graphql"
 	routes_v3_codebase "getsturdy.com/api/pkg/codebase/routes"
 	service_codebase "getsturdy.com/api/pkg/codebase/service"
-	db_comments "getsturdy.com/api/pkg/comments/db"
-	service_comments "getsturdy.com/api/pkg/comments/service"
-	"getsturdy.com/api/pkg/db"
+	"getsturdy.com/api/pkg/di"
 	"getsturdy.com/api/pkg/events"
-	graphql_file "getsturdy.com/api/pkg/file/graphql"
 	gqlerrors "getsturdy.com/api/pkg/graphql/errors"
 	"getsturdy.com/api/pkg/graphql/resolvers"
-	"getsturdy.com/api/pkg/internal/sturdytest"
-	"getsturdy.com/api/pkg/notification/sender"
-	"getsturdy.com/api/pkg/queue"
-	db_review "getsturdy.com/api/pkg/review/db"
-	graphql_review "getsturdy.com/api/pkg/review/graphql"
 	db_snapshots "getsturdy.com/api/pkg/snapshots/db"
 	"getsturdy.com/api/pkg/snapshots/snapshotter"
-	worker_snapshots "getsturdy.com/api/pkg/snapshots/worker"
-	db_statuses "getsturdy.com/api/pkg/statuses/db"
-	graphql_statuses "getsturdy.com/api/pkg/statuses/graphql"
-	service_statuses "getsturdy.com/api/pkg/statuses/service"
-	db_suggestion "getsturdy.com/api/pkg/suggestions/db"
-	service_suggestion "getsturdy.com/api/pkg/suggestions/service"
 	"getsturdy.com/api/pkg/users"
 	db_user "getsturdy.com/api/pkg/users/db"
-	service_user "getsturdy.com/api/pkg/users/service"
 	"getsturdy.com/api/pkg/view"
 	db_view "getsturdy.com/api/pkg/view/db"
-	graphql_view "getsturdy.com/api/pkg/view/graphql"
 	routes_v3_view "getsturdy.com/api/pkg/view/routes"
 	"getsturdy.com/api/pkg/workspace"
-	db_activity "getsturdy.com/api/pkg/workspace/activity/db"
-	activity_sender "getsturdy.com/api/pkg/workspace/activity/sender"
-	service_activity "getsturdy.com/api/pkg/workspace/activity/service"
 	db_workspace "getsturdy.com/api/pkg/workspace/db"
-	graphql_workspace "getsturdy.com/api/pkg/workspace/graphql"
-	ws_meta "getsturdy.com/api/pkg/workspace/meta"
 	routes_v3_workspace "getsturdy.com/api/pkg/workspace/routes"
 	service_workspace "getsturdy.com/api/pkg/workspace/service"
-	db_workspace_watchers "getsturdy.com/api/pkg/workspace/watchers/db"
-	graphql_workspace_watchers "getsturdy.com/api/pkg/workspace/watchers/graphql"
-	service_workspace_watchers "getsturdy.com/api/pkg/workspace/watchers/service"
 	"getsturdy.com/api/vcs/executor"
 	"getsturdy.com/api/vcs/provider"
 
@@ -76,206 +45,44 @@ func TestRevertChangeFromSnapshot(t *testing.T) {
 		t.SkipNow()
 	}
 
-	reposBasePath := os.TempDir()
-	repoProvider := provider.New(reposBasePath, "localhost:8888")
+	type deps struct {
+		dig.In
+		UserRepo              db_user.Repository
+		CodebaseRootResolver  resolvers.CodebaseRootResolver
+		WorkspaceRootResolver resolvers.WorkspaceRootResolver
 
-	d, err := db.Setup(
-		sturdytest.PsqlDbSourceForTesting(),
-	)
-	if err != nil {
-		panic(err)
+		CodebaseService  *service_codebase.Service
+		WorkspaceService service_workspace.Service
+		GitSnapshotter   snapshotter.Snapshotter
+		RepoProvider     provider.RepoProvider
+		
+		CodebaseUserRepo db_codebase.CodebaseUserRepository
+		WorkspaceRepo    db_workspace.Repository
+		ViewRepo         db_view.Repository
+		SnapshotRepo     db_snapshots.Repository
+		ExecutorProvider executor.Provider
+		EventsSender     events.EventSender
+
+		Logger          *zap.Logger
+		AnalyticsClient analytics.Client
 	}
 
-	logger := zap.NewNop()
-	postHogClient := disabled.NewClient()
+	var d deps
+	if !assert.NoError(t, di.Init(&d, module)) {
+		t.FailNow()
+	}
 
-	userRepo := db_user.NewRepo(d)
-	codebaseRepo := db_codebase.NewRepo(d)
-	codebaseUserRepo := db_codebase.NewCodebaseUserRepo(d)
-	workspaceRepo := db_workspace.NewRepo(d)
-	viewRepo := db_view.NewRepo(d)
-	changeRepo := db_change.NewRepo(d)
-	snapshotRepo := db_snapshots.NewRepo(d)
-	commentRepo := db_comments.NewRepo(d)
-	workspaceActivityRepo := db_activity.NewActivityRepo(d)
-	workspaceActivityReadsRepo := db_activity.NewActivityReadsRepo(d)
-	viewEvents := events.NewInMemory()
-	executorProvider := executor.NewProvider(logger, repoProvider)
-	reviewRepo := db_review.NewReviewRepository(d)
-	eventsSender := events.NewSender(codebaseUserRepo, workspaceRepo, viewEvents)
-	gitSnapshotter := snapshotter.NewGitSnapshotter(snapshotRepo, workspaceRepo, workspaceRepo, viewRepo, eventsSender, executorProvider, logger)
-	snapshotPublisher := worker_snapshots.NewSync(gitSnapshotter)
-	activityService := service_activity.New(workspaceActivityReadsRepo, eventsSender)
-	activitySender := activity_sender.NewActivitySender(codebaseUserRepo, workspaceActivityRepo, activityService, eventsSender)
-	suggestionRepo := db_suggestion.New(d)
-	notificationSender := sender.NewNoopNotificationSender()
-	commentsService := service_comments.New(commentRepo)
-	aclRepo := db_acl.NewACLRepository(d)
+	repoProvider := d.RepoProvider
+	userRepo := d.UserRepo
+	workspaceService := d.WorkspaceService
+	workspaceRepo := d.WorkspaceRepo
 
-	queue := queue.NewNoop()
-	buildQueue := workers_ci.New(zap.NewNop(), queue, nil)
-	userService := service_user.New(zap.NewNop(), userRepo, postHogClient)
+	createCodebaseRoute := routes_v3_codebase.Create(d.Logger, d.CodebaseService)
+	createWorkspaceRoute := routes_v3_workspace.Create(d.Logger, d.WorkspaceService, d.CodebaseUserRepo)
+	createViewRoute := routes_v3_view.Create(d.Logger, d.ViewRepo, d.CodebaseUserRepo, d.AnalyticsClient, d.WorkspaceRepo, d.GitSnapshotter, d.SnapshotRepo, d.WorkspaceRepo, d.ExecutorProvider, d.EventsSender)
 
-	aclProvider := provider_acl.New(aclRepo, codebaseUserRepo, userRepo)
-
-	workspaceWriter := ws_meta.NewWriterWithEvents(logger, workspaceRepo, eventsSender)
-	changeService := service_change.New(aclProvider, changeRepo, logger)
-	workspaceService := service_workspace.New(
-		logger,
-		postHogClient,
-
-		workspaceWriter,
-		workspaceRepo,
-
-		userRepo,
-		reviewRepo,
-
-		commentsService,
-		changeService,
-
-		activitySender,
-		executorProvider,
-		eventsSender,
-		snapshotPublisher,
-		gitSnapshotter,
-		buildQueue,
-	)
-
-	codebaseService := service_codebase.New(codebaseRepo, codebaseUserRepo, workspaceService, nil, logger, executorProvider, postHogClient, eventsSender)
-
-	suggestionsService := service_suggestion.New(
-		logger,
-		suggestionRepo,
-		workspaceService,
-		executorProvider,
-		gitSnapshotter,
-		postHogClient,
-		notificationSender,
-		eventsSender,
-	)
-
-	authService := service_auth.New(codebaseService, userService, workspaceService, aclProvider, nil /*organizationService*/)
-
-	createCodebaseRoute := routes_v3_codebase.Create(logger, codebaseService)
-	createWorkspaceRoute := routes_v3_workspace.Create(logger, workspaceService, codebaseUserRepo)
-	createViewRoute := routes_v3_view.Create(logger, viewRepo, codebaseUserRepo, postHogClient, workspaceRepo, gitSnapshotter, snapshotRepo, workspaceWriter, executorProvider, eventsSender)
-
-	workspaceWatchersRootResolver := new(resolvers.WorkspaceWatcherRootResolver)
-	workspaceWatcherRepo := db_workspace_watchers.NewInMemory()
-	workspaceWatchersService := service_workspace_watchers.New(workspaceWatcherRepo, eventsSender)
-
-	reviewRootResolver := graphql_review.New(
-		logger,
-		reviewRepo,
-		nil,
-		authService,
-		nil,
-		nil,
-		eventsSender,
-		viewEvents,
-		nil,
-		nil,
-		workspaceWatchersService,
-	)
-
-	statusesRepo := db_statuses.New(d)
-	statusesServcie := service_statuses.New(logger, statusesRepo, eventsSender)
-	statusesRootResolver := new(resolvers.StatusesRootResolver)
-
-	changeRootResolver := graphql_change.NewResolver(
-		changeService,
-		commentRepo,
-		authService,
-		nil, // commentResolver
-		nil, // authorResolver
-		statusesRootResolver,
-		nil, // downloadsResovler
-		executorProvider,
-		logger,
-	)
-
-	fileRootResolver := graphql_file.NewFileRootResolver(executorProvider, authService)
-
-	workspaceRootResolver := graphql_workspace.NewResolver(
-		workspaceRepo,
-		codebaseRepo,
-		viewRepo,
-		nil, // commentRepo
-		nil, // snapshotRepo
-		nil, // codebaseResolver
-		nil, // authorResolver
-		nil, // viewResolver
-		nil, // commentResolver
-		nil, // prResolver
-		changeRootResolver,
-		nil, // workspaceActivityResolver
-		reviewRootResolver,
-		nil, // presenseRootResolver
-		nil, // suggestitonsRootResolver
-		*statusesRootResolver,
-		*workspaceWatchersRootResolver,
-		suggestionsService,
-		workspaceService,
-		authService,
-		logger,
-		viewEvents,
-		workspaceWriter,
-		executorProvider,
-		eventsSender,
-		gitSnapshotter,
-	)
-
-	*workspaceWatchersRootResolver = graphql_workspace_watchers.NewRootResolver(
-		logger,
-		workspaceWatchersService,
-		workspaceService,
-		authService,
-		viewEvents,
-		nil,
-		&workspaceRootResolver,
-	)
-
-	changeDecorator := decorate.New(changeRepo, userService, codebaseService, executorProvider, logger)
-
-	codebaseRootResolver := graphql_codebase.NewCodebaseRootResolver(
-		codebaseRepo,
-		codebaseUserRepo,
-		viewRepo,
-		workspaceRepo,
-		userRepo,
-
-		nil,
-		nil,
-		nil,
-		nil,
-		changeRootResolver,
-		fileRootResolver,
-		nil, // instantIntegrationRootResolver
-		nil,
-		nil,
-
-		logger,
-		nil,
-		nil,
-		postHogClient,
-		executorProvider,
-
-		authService,
-		codebaseService,
-		nil,
-		changeDecorator,
-	)
-
-	*statusesRootResolver = graphql_statuses.New(
-		logger,
-		statusesServcie,
-		changeService,
-		workspaceService,
-		authService,
-		changeRootResolver,
-		nil, // github pr resolver
-		viewEvents,
-	)
+	workspaceRootResolver := d.WorkspaceRootResolver
+	codebaseRootResolver := d.CodebaseRootResolver
 
 	createUser := users.User{ID: uuid.New().String(), Name: "Test", Email: uuid.New().String() + "@getsturdy.com"}
 	assert.NoError(t, userRepo.Create(&createUser))
@@ -322,7 +129,7 @@ func TestRevertChangeFromSnapshot(t *testing.T) {
 
 	for _, ch := range changes {
 		// Make changes in the view
-		err = ioutil.WriteFile(path.Join(viewPath, ch.file), []byte(ch.contents), 0o666)
+		err := ioutil.WriteFile(path.Join(viewPath, ch.file), []byte(ch.contents), 0o666)
 		assert.NoError(t, err)
 
 		// Get diff
@@ -417,226 +224,45 @@ func TestRevertChangeFromView(t *testing.T) {
 		t.SkipNow()
 	}
 
-	reposBasePath := os.TempDir()
-	repoProvider := provider.New(reposBasePath, "localhost:8888")
+	type deps struct {
+		dig.In
+		UserRepo              db_user.Repository
+		CodebaseRootResolver  resolvers.CodebaseRootResolver
+		WorkspaceRootResolver resolvers.WorkspaceRootResolver
+		ViewRootResolver      resolvers.ViewRootResolver
 
-	d, err := db.Setup(
-		sturdytest.PsqlDbSourceForTesting(),
-	)
-	if err != nil {
-		panic(err)
+		CodebaseService  *service_codebase.Service
+		WorkspaceService service_workspace.Service
+		GitSnapshotter   snapshotter.Snapshotter
+		RepoProvider     provider.RepoProvider
+
+		CodebaseUserRepo db_codebase.CodebaseUserRepository
+		WorkspaceRepo    db_workspace.Repository
+		ViewRepo         db_view.Repository
+		SnapshotRepo     db_snapshots.Repository
+		ExecutorProvider executor.Provider
+		EventsSender     events.EventSender
+
+		Logger          *zap.Logger
+		AnalyticsClient analytics.Client
 	}
 
-	logger := zap.NewNop()
-	postHogClient := disabled.NewClient()
+	var d deps
+	if !assert.NoError(t, di.Init(&d, module)) {
+		t.FailNow()
+	}
 
-	userRepo := db_user.NewRepo(d)
-	codebaseRepo := db_codebase.NewRepo(d)
-	codebaseUserRepo := db_codebase.NewCodebaseUserRepo(d)
-	workspaceRepo := db_workspace.NewRepo(d)
-	viewRepo := db_view.NewRepo(d)
-	changeRepo := db_change.NewRepo(d)
-	snapshotRepo := db_snapshots.NewRepo(d)
-	commentRepo := db_comments.NewRepo(d)
-	workspaceActivityRepo := db_activity.NewActivityRepo(d)
-	workspaceActivityReadsRepo := db_activity.NewActivityReadsRepo(d)
-	viewEvents := events.NewInMemory()
-	executorProvider := executor.NewProvider(logger, repoProvider)
-	reviewRepo := db_review.NewReviewRepository(d)
-	eventsSender := events.NewSender(codebaseUserRepo, workspaceRepo, viewEvents)
-	gitSnapshotter := snapshotter.NewGitSnapshotter(snapshotRepo, workspaceRepo, workspaceRepo, viewRepo, eventsSender, executorProvider, logger)
-	snapshotPublisher := worker_snapshots.NewSync(gitSnapshotter)
-	activityService := service_activity.New(workspaceActivityReadsRepo, eventsSender)
-	activitySender := activity_sender.NewActivitySender(codebaseUserRepo, workspaceActivityRepo, activityService, eventsSender)
-	suggestionRepo := db_suggestion.New(d)
-	notificationSender := sender.NewNoopNotificationSender()
-	commentsService := service_comments.New(commentRepo)
-	aclRepo := db_acl.NewACLRepository(d)
+	repoProvider := d.RepoProvider
+	userRepo := d.UserRepo
+	workspaceService := d.WorkspaceService
+	viewRootResolver := d.ViewRootResolver
 
-	queue := queue.NewNoop()
-	buildQueue := workers_ci.New(zap.NewNop(), queue, nil)
-	userService := service_user.New(zap.NewNop(), userRepo, postHogClient)
+	createCodebaseRoute := routes_v3_codebase.Create(d.Logger, d.CodebaseService)
+	createWorkspaceRoute := routes_v3_workspace.Create(d.Logger, d.WorkspaceService, d.CodebaseUserRepo)
+	createViewRoute := routes_v3_view.Create(d.Logger, d.ViewRepo, d.CodebaseUserRepo, d.AnalyticsClient, d.WorkspaceRepo, d.GitSnapshotter, d.SnapshotRepo, d.WorkspaceRepo, d.ExecutorProvider, d.EventsSender)
 
-	workspaceWriter := ws_meta.NewWriterWithEvents(logger, workspaceRepo, eventsSender)
-	changeService := service_change.New(nil, changeRepo, logger)
-
-	workspaceService := service_workspace.New(
-		logger,
-		postHogClient,
-
-		workspaceWriter,
-		workspaceRepo,
-
-		userRepo,
-		reviewRepo,
-
-		commentsService,
-		changeService,
-
-		activitySender,
-		executorProvider,
-		eventsSender,
-		snapshotPublisher,
-		gitSnapshotter,
-		buildQueue,
-	)
-
-	codebaseService := service_codebase.New(codebaseRepo, codebaseUserRepo, workspaceService, nil, logger, executorProvider, postHogClient, eventsSender)
-
-	suggestionsService := service_suggestion.New(
-		logger,
-		suggestionRepo,
-		workspaceService,
-		executorProvider,
-		gitSnapshotter,
-		postHogClient,
-		notificationSender,
-		eventsSender,
-	)
-
-	aclProvider := provider_acl.New(aclRepo, codebaseUserRepo, userRepo)
-
-	authService := service_auth.New(codebaseService, userService, workspaceService, aclProvider, nil /*organizationService*/)
-
-	createCodebaseRoute := routes_v3_codebase.Create(logger, codebaseService)
-	createWorkspaceRoute := routes_v3_workspace.Create(logger, workspaceService, codebaseUserRepo)
-	createViewRoute := routes_v3_view.Create(logger, viewRepo, codebaseUserRepo, postHogClient, workspaceRepo, gitSnapshotter, snapshotRepo, workspaceWriter, executorProvider, eventsSender)
-
-	workspaceWatchersRootResolver := new(resolvers.WorkspaceWatcherRootResolver)
-	workspaceWatcherRepo := db_workspace_watchers.NewInMemory()
-	workspaceWatchersService := service_workspace_watchers.New(workspaceWatcherRepo, eventsSender)
-
-	reviewRootResolver := graphql_review.New(
-		logger,
-		reviewRepo,
-		nil,
-		authService,
-		nil,
-		nil,
-		eventsSender,
-		viewEvents,
-		nil,
-		nil,
-		workspaceWatchersService,
-	)
-
-	statusesRepo := db_statuses.New(d)
-	statusesServcie := service_statuses.New(logger, statusesRepo, eventsSender)
-	statusesRootResolver := new(resolvers.StatusesRootResolver)
-
-	changeRootResolver := graphql_change.NewResolver(
-		changeService,
-		commentRepo,
-		authService,
-		nil, // commentResolver
-		nil, // authorResolver
-		statusesRootResolver,
-		nil, // downloadsResolver
-		executorProvider,
-		logger,
-	)
-
-	fileRootResolver := graphql_file.NewFileRootResolver(executorProvider, authService)
-
-	workspaceRootResolver := graphql_workspace.NewResolver(
-		workspaceRepo,
-		codebaseRepo,
-		viewRepo,
-		nil, // commentRepo
-		nil, // snapshotRepo
-		nil, // codebaseResolver
-		nil, // authorResolver
-		nil, // viewResolver
-		nil, // commentResolver
-		nil, // prResolver
-		changeRootResolver,
-		nil, // workspaceActivityResolver
-		reviewRootResolver,
-		nil, // presenseRootResolver
-		nil, // suggestitonsRootResolver
-		*statusesRootResolver,
-		*workspaceWatchersRootResolver,
-		suggestionsService,
-		workspaceService,
-		authService,
-		logger,
-		viewEvents,
-		workspaceWriter,
-		executorProvider,
-		eventsSender,
-		gitSnapshotter,
-	)
-
-	*workspaceWatchersRootResolver = graphql_workspace_watchers.NewRootResolver(
-		logger,
-		workspaceWatchersService,
-		workspaceService,
-		authService,
-		viewEvents,
-		nil,
-		&workspaceRootResolver,
-	)
-
-	viewRootResolver := graphql_view.NewResolver(
-		viewRepo,
-		workspaceRepo,
-		gitSnapshotter,
-		snapshotRepo,
-		nil,
-		nil,
-		workspaceWriter,
-		viewEvents,
-		eventsSender,
-		executorProvider,
-		logger,
-		nil,
-		workspaceWatchersService,
-		postHogClient,
-		nil,
-		authService,
-	)
-
-	changeDecorator := decorate.New(changeRepo, userService, codebaseService, executorProvider, logger)
-
-	codebaseRootResolver := graphql_codebase.NewCodebaseRootResolver(
-		codebaseRepo,
-		codebaseUserRepo,
-		viewRepo,
-		workspaceRepo,
-		userRepo,
-
-		nil,
-		nil,
-		nil,
-		nil,
-		changeRootResolver,
-		fileRootResolver,
-		nil, // instantIntegrationRootResolver
-		nil,
-		nil,
-
-		logger,
-		nil,
-		nil,
-		postHogClient,
-		executorProvider,
-
-		authService,
-		codebaseService,
-		nil,
-		changeDecorator,
-	)
-
-	*statusesRootResolver = graphql_statuses.New(
-		logger,
-		statusesServcie,
-		changeService,
-		workspaceService,
-		authService,
-		changeRootResolver,
-		nil, // github pr resolver
-		viewEvents,
-	)
+	workspaceRootResolver := d.WorkspaceRootResolver
+	codebaseRootResolver := d.CodebaseRootResolver
 
 	createUser := users.User{ID: uuid.New().String(), Name: "Test", Email: uuid.New().String() + "@getsturdy.com"}
 	assert.NoError(t, userRepo.Create(&createUser))
@@ -683,7 +309,7 @@ func TestRevertChangeFromView(t *testing.T) {
 
 	for _, ch := range changes {
 		// Make changes in the view
-		err = ioutil.WriteFile(path.Join(viewPath, ch.file), []byte(ch.contents), 0o666)
+		err := ioutil.WriteFile(path.Join(viewPath, ch.file), []byte(ch.contents), 0o666)
 		assert.NoError(t, err)
 
 		// Get diff
