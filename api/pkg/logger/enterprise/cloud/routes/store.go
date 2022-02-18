a7ef05e1e6647b3ba666f6d6df4a88b7224d713d
Nikita Galaiko <nikita@galaiko.rocks>
2022-02-18 15:35:22 +0000

    api/logger: fix sentry proxy format to capture errors
    --- sturdy ---
    user_id: cc33c18b-27e5-470d-8589-f9391ca37590
    view_id: 2c064d23-2410-4e86-939f-91c56dae2fae
    workspace_id: 855151f1-f5c7-41f7-b2e0-833fb3f35550

--- api/pkg/logger/enterprise/cloud/routes/store.go
+++ api/pkg/logger/enterprise/cloud/routes/store.go
@@ -11,22 +11,28 @@ import (
 )
 
 func Store(logger *zap.Logger, sentryClient *raven.Client) http.HandlerFunc {
+	type request struct {
+		raven.Packet
+
+		Exception *struct {
+			raven.Exception
+			Values []*raven.Exception `json:"values"`
+		} `json:"exception"`
+	}
 	return func(w http.ResponseWriter, r *http.Request) {
 		contentType := r.Header.Get("Content-Type")
+		packet := &request{}
 		switch contentType {
 		case "application/json":
-			packet := &raven.Packet{}
 			if err := json.NewDecoder(r.Body).Decode(packet); err != nil {
 				logger.Error("failed to decode sentry packet", zap.Error(err))
 				w.WriteHeader(http.StatusBadRequest)
 				return
 			}
 			r.Body.Close()
-			sentryClient.Capture(packet, nil)
 		case "application/octet-stream":
 			b64 := base64.NewDecoder(base64.StdEncoding, r.Body)
 			deflate, _ := zlib.NewReader(b64)
-			packet := &raven.Packet{}
 			if err := json.NewDecoder(deflate).Decode(packet); err != nil {
 				logger.Error("failed to decode sentry packet", zap.Error(err))
 				w.WriteHeader(http.StatusBadRequest)
@@ -34,11 +40,21 @@ func Store(logger *zap.Logger, sentryClient *raven.Client) http.HandlerFunc {
 			}
 			deflate.Close()
 			r.Body.Close()
-			sentryClient.Capture(packet, nil)
 		default:
 			logger.Error("failed to decode sentry packet: unexpected content-type", zap.String("content-type", contentType))
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
+
+		if packet.Exception != nil {
+			packet.Interfaces = append(packet.Interfaces, &packet.Exception.Exception)
+			if len(packet.Exception.Values) > 0 {
+				packet.Interfaces = append(packet.Interfaces, &raven.Exceptions{
+					Values: packet.Exception.Values,
+				})
+			}
+		}
+
+		sentryClient.Capture(&packet.Packet, nil)
 	}
 }
