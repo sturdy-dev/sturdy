540516ef14eb81de1031fc386dcd2183aedeee38
Gustav Westling <gustav@getsturdy.com>
2022-02-22 12:42:26 +0000

    web/Index: set explicit sizes on images
    --- sturdy ---
    user_id: 847dfd0c-49bf-40c5-8870-74a12fca0d60
    view_id: 128badfe-5c58-487e-adfd-c4f1e4fd160f
    workspace_id: 87890dbf-6c59-41c3-8923-085553b7ede5

--- web/src/organisms/NavigationHeader.vue
+++ web/src/organisms/NavigationHeader.vue
@@ -14,7 +14,13 @@
         <div class="flex items-center">
           <div class="flex-shrink-0">
             <router-link :to="{ name: 'v2Index' }">
-              <img src="../pages/landing/assets/logotype.svg" alt="Sturdy logotype" class="h-7" />
+              <img
+                src="../pages/landing/assets/logotype.svg"
+                alt="Sturdy logotype"
+                class="h-7"
+                height="28"
+                width="110"
+              />
             </router-link>
           </div>
         </div>
