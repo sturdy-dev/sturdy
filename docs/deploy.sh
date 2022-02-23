939df459e8d4cc33c2338fc18690214fd570278e
Gustav Westling <gustav@getsturdy.com>
2022-02-23 08:17:35 +0000

    docs: use spectaql to generate the documentation site SpectaQL can generate the documentation directly from production introspection endpoint, which makes this site easy to maintain.
    --- sturdy ---
    user_id: 847dfd0c-49bf-40c5-8870-74a12fca0d60
    view_id: 128badfe-5c58-487e-adfd-c4f1e4fd160f
    workspace_id: 1f7085f8-6b2d-4f56-a6fa-1c8115a1f309

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -euo pipefail
+
 yarn install
-yarn run graphdoc -s ../pkg/graphql/schema.graphql -b "https://schema.getsturdy.com/" -o public --force
+yarn spectaql spectaql.yml
 aws s3 sync ./public/ s3://schema.getsturdy.com/
-aws s3 cp ../pkg/graphql/schema.graphql s3://schema.getsturdy.com/sturdy.graphql
+# aws s3 cp ../pkg/graphql/schema.graphql s3://schema.getsturdy.com/sturdy.graphql
 aws cloudfront create-invalidation --distribution-id "E1GIL34XU5VF0F" --paths "/*"
