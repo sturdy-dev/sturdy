7a61d58cb02a8647d2cad6006253eb69a5144179
Nikita Galaiko <nikita@galaiko.rocks>
2022-02-28 10:35:33 +0000

    web/changelogchange: perttify + add option to show descriptions
    --- sturdy ---
    user_id: cc33c18b-27e5-470d-8589-f9391ca37590
    view_id: 2c064d23-2410-4e86-939f-91c56dae2fae
    workspace_id: 6ff84ac8-3826-42bc-840d-6a633319a3e8

---
+++ web/src/atoms/RelativeTime.vue
@@ -0,0 +1,21 @@
+<template>
+  <time :datetime="date.toISOString()" :title="date.toLocaleString()">{{ ago }}</time>
+</template>
+
+<script lang="ts">
+import time from '../time'
+
+export default {
+  props: {
+    date: {
+      type: Date,
+      required: true,
+    },
+  },
+  computed: {
+    ago() {
+      return time.getRelativeTime(this.date)
+    },
+  },
+}
+</script>
