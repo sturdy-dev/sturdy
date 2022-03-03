01a560f80cbafccdf09876bfee45906bbe9c7d28
Gustav Westling <gustav@getsturdy.com>
2022-03-03 15:58:56 +0000

    api/pkg/workspaces: remove unused columns
    --- sturdy ---
    user_id: 847dfd0c-49bf-40c5-8870-74a12fca0d60
    workspace_id: bb44b07a-2141-47b2-9ec2-e8a3ab96ceb8

--- api/pkg/workspaces/db/db.go
+++ api/pkg/workspaces/db/db.go
@@ -30,7 +30,7 @@ func (r *repo) Create(entity workspaces.Workspace) error {
 
 func (r *repo) Get(id string) (*workspaces.Workspace, error) {
 	var entity workspaces.Workspace
-	err := r.db.Get(&entity, `SELECT id, user_id, codebase_id, name, ready_for_review_change, approved_change, created_at, last_landed_at, archived_at, unarchived_at, updated_at, draft_description, view_id, latest_snapshot_id, up_to_date_with_trunk, head_change_id, head_change_computed, diffs_count
+	err := r.db.Get(&entity, `SELECT id, user_id, codebase_id, name,  created_at, last_landed_at, archived_at, unarchived_at, updated_at, draft_description, view_id, latest_snapshot_id, up_to_date_with_trunk, head_change_id, head_change_computed, diffs_count
 	FROM workspaces
 	WHERE id=$1`, id)
 	if err != nil {
@@ -40,7 +40,7 @@ func (r *repo) Get(id string) (*workspaces.Workspace, error) {
 }
 
 func (r *repo) ListByCodebaseIDs(codebaseIDs []string, includeArchived bool) ([]*workspaces.Workspace, error) {
-	q := `SELECT id, user_id, codebase_id, name, ready_for_review_change, approved_change, created_at, last_landed_at, archived_at, unarchived_at, updated_at, draft_description, view_id, latest_snapshot_id, up_to_date_with_trunk, head_change_id, head_change_computed, diffs_count
+	q := `SELECT id, user_id, codebase_id, name, created_at, last_landed_at, archived_at, unarchived_at, updated_at, draft_description, view_id, latest_snapshot_id, up_to_date_with_trunk, head_change_id, head_change_computed, diffs_count
 	FROM workspaces
 	WHERE codebase_id IN(?)`
 
@@ -63,7 +63,7 @@ func (r *repo) ListByCodebaseIDs(codebaseIDs []string, includeArchived bool) ([]
 }
 
 func (r *repo) ListByCodebaseIDsAndUserID(codebaseIDs []string, userID string) ([]*workspaces.Workspace, error) {
-	query, args, err := sqlx.In(`SELECT id, user_id, codebase_id, name, ready_for_review_change, approved_change, created_at, last_landed_at, archived_at, unarchived_at, updated_at, draft_description, view_id, latest_snapshot_id, up_to_date_with_trunk, head_change_id, diffs_count
+	query, args, err := sqlx.In(`SELECT id, user_id, codebase_id, name, created_at, last_landed_at, archived_at, unarchived_at, updated_at, draft_description, view_id, latest_snapshot_id, up_to_date_with_trunk, head_change_id, diffs_count
 	FROM workspaces
 	WHERE codebase_id IN(?)
 	  AND user_id = ?
@@ -86,8 +86,6 @@ func (r *repo) ListByCodebaseIDsAndUserID(codebaseIDs []string, userID string) (
 func (r *repo) Update(ctx context.Context, entity *workspaces.Workspace) error {
 	if _, err := r.db.NamedExecContext(ctx, `UPDATE workspaces
 		SET name = :name,
-			ready_for_review_change = :ready_for_review_change,
-			approved_change = :approved_change,
 		    last_landed_at = :last_landed_at,
 		    archived_at = :archived_at,
 		    unarchived_at = :unarchived_at,
@@ -116,7 +114,7 @@ func (r *repo) UnsetUpToDateWithTrunkForAllInCodebase(codebaseID string) error {
 func (r *repo) GetByViewID(viewID string, includeArchived bool) (*workspaces.Workspace, error) {
 	var entity workspaces.Workspace
 
-	q := `SELECT id, user_id, codebase_id, name, ready_for_review_change, approved_change, created_at, last_landed_at, archived_at, unarchived_at, updated_at, draft_description, view_id, latest_snapshot_id, up_to_date_with_trunk, head_change_id, head_change_computed, diffs_count
+	q := `SELECT id, user_id, codebase_id, name, created_at, last_landed_at, archived_at, unarchived_at, updated_at, draft_description, view_id, latest_snapshot_id, up_to_date_with_trunk, head_change_id, head_change_computed, diffs_count
 		FROM workspaces
 		WHERE view_id=$1`
 
@@ -138,8 +136,6 @@ func (r *repo) GetBySnapshotID(snapshotID string) (*workspaces.Workspace, error)
 			user_id,
 			codebase_id,
 			name,
-			ready_for_review_change,
-			approved_change,
 			created_at,
 			last_landed_at,
 			archived_at,
