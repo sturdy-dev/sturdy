faa6c944cfc6ee6c4e2d2154bcf5f59d02724f87
Nikita Galaiko <nikita@galaiko.rocks>
2022-03-04 16:43:21 +0000

    api/events: v2 draft
    --- sturdy ---
    user_id: cc33c18b-27e5-470d-8589-f9391ca37590
    view_id: 2c064d23-2410-4e86-939f-91c56dae2fae
    workspace_id: eb580570-bead-44f2-87c7-b5910cbff1c8

---
+++ api/pkg/events/v2/event.go
@@ -0,0 +1,102 @@
+package events
+
+import (
+	"getsturdy.com/api/pkg/codebase"
+	"getsturdy.com/api/pkg/github"
+	"getsturdy.com/api/pkg/notification"
+	"getsturdy.com/api/pkg/onboarding"
+	"getsturdy.com/api/pkg/organization"
+	"getsturdy.com/api/pkg/review"
+	"getsturdy.com/api/pkg/statuses"
+	"getsturdy.com/api/pkg/view"
+	"getsturdy.com/api/pkg/workspaces"
+	"getsturdy.com/api/pkg/workspaces/watchers"
+)
+
+type Type uint
+
+const (
+	TypeUndefined Type = iota
+
+	CodebaseEvent
+	CodebaseUpdated
+
+	ViewUpdated
+	ViewStatusUpdated
+
+	WorkspaceUpdated
+	WorkspaceUpdatedComments
+	WorkspaceUpdatedReviews
+	WorkspaceUpdatedActivity
+	WorkspaceUpdatedSnapshot
+	WorkspaceUpdatedPresence
+	WorkspaceUpdatedSuggestion
+	WorkspaceWatchingStatusUpdated
+
+	ReviewUpdated
+	GitHubPRUpdated
+	NotificationEvent
+	StatusUpdated
+	CompletedOnboardingStep
+	OrganizationUpdated
+)
+
+func (t Type) String() string {
+	switch t {
+	case TypeUndefined:
+		return "TypeUndefined"
+	case CodebaseEvent:
+		return "CodebaseEvent"
+	case CodebaseUpdated:
+		return "CodebaseUpdated"
+	case ViewUpdated:
+		return "ViewUpdated"
+	case ViewStatusUpdated:
+		return "ViewStatusUpdated"
+	case WorkspaceUpdated:
+		return "WorkspaceUpdated"
+	case WorkspaceUpdatedComments:
+		return "WorkspaceUpdatedComments"
+	case WorkspaceUpdatedReviews:
+		return "WorkspaceUpdatedReviews"
+	case ReviewUpdated:
+		return "ReviewUpdated"
+	case WorkspaceUpdatedActivity:
+		return "WorkspaceUpdatedActivity"
+	case WorkspaceUpdatedSnapshot:
+		return "WorkspaceUpdatedSnapshot"
+	case WorkspaceUpdatedPresence:
+		return "WorkspaceUpdatedPresence"
+	case WorkspaceUpdatedSuggestion:
+		return "WorkspaceUpdatedSuggestion"
+	case GitHubPRUpdated:
+		return "GitHubPRUpdated"
+	case NotificationEvent:
+		return "NotificationEvent"
+	case StatusUpdated:
+		return "StatusUpdated"
+	case CompletedOnboardingStep:
+		return "CompletedOnboardingStep"
+	case WorkspaceWatchingStatusUpdated:
+		return "WorkspaceWatchingStatusUpdated"
+	case OrganizationUpdated:
+		return "OrganizationUpdated"
+	default:
+		return "Unknown"
+	}
+}
+
+type event struct {
+	Type Type
+
+	Codebase          *codebase.Codebase
+	View              *view.View
+	Workspace         *workspaces.Workspace
+	Review            *review.Review
+	GitHubPullRequest *github.GitHubPullRequest
+	Notification      *notification.Notification
+	Status            *statuses.Status
+	OnboardingStep    *onboarding.Step
+	WorkspaceWatcher  *watchers.Watcher
+	Organization      *organization.Organization
+}
