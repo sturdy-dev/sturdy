8563d07570f94d5afeae3d89d092aa3c714d9661
Gustav Westling <gustav@getsturdy.com>
2022-03-10 17:08:14 +0000

    web/Index: add Product Hunt badge
    --- sturdy ---
    user_id: 847dfd0c-49bf-40c5-8870-74a12fca0d60
    view_id: 128badfe-5c58-487e-adfd-c4f1e4fd160f
    workspace_id: 012f3050-7cc4-433f-90d6-6f3ebcfddaaf

--- web/src/pages/landing/Index.vue
+++ web/src/pages/landing/Index.vue
@@ -741,6 +741,17 @@
                   height="24"
                 />
               </a>
+
+              <a
+                href="https://www.producthunt.com/posts/sturdy?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-sturdy"
+                target="_blank"
+                ><img
+                  src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=335256&theme=light"
+                  alt="Sturdy - Simple&#0032;&#0038;&#0032;efficient&#0032;code&#0032;collaboration | Product Hunt"
+                  style="width: 250px; height: 54px"
+                  width="250"
+                  height="54"
+              /></a>
             </div>
           </div>
         </div>
