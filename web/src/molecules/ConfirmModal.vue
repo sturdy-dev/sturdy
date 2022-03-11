728731be1e768944697c486d07f08e769b80a966
Nikita Galaiko <nikita@galaiko.rocks>
2022-03-11 15:32:22 +0000

    web/workspaces: fix archive modal z index
    --- sturdy ---
    user_id: cc33c18b-27e5-470d-8589-f9391ca37590
    view_id: 2c064d23-2410-4e86-939f-91c56dae2fae
    workspace_id: 528b4104-d34f-43b9-b6a3-a4e604a06ebf

--- web/src/molecules/ConfirmModal.vue
+++ web/src/molecules/ConfirmModal.vue
@@ -1,6 +1,6 @@
 <template>
   <TransitionRoot as="template" :show="show">
-    <Dialog as="div" class="fixed z-10 inset-0 overflow-y-auto" @close="close">
+    <Dialog as="div" class="fixed z-20 inset-0 overflow-y-auto" @close="close">
       <div
         class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
       >
