b42c379615a366e0572497deeabe43875091c1c4
Nikita Galaiko <nikita@galaiko.rocks>
2022-03-21 16:00:19 +0000

    web/workspace: extract workspace details
    Created with Sturdy

--- web/src/components/workspace/details/BasedOn.vue
+++ web/src/components/workspace/details/BasedOn.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="space-y-5 md:flex-1">
     <router-link
-      v-if="data.workspace.headChange"
+      v-if="workspace.headChange"
       :to="{
         name: 'codebaseChange',
         params: {
           codebaseSlug: codebaseSlug,
-          id: data.workspace.headChange.id,
+          id: workspace.headChange.id,
         },
       }"
       class="flex items-center space-x-2"
     >
-      <Avatar size="5" :author="data.workspace.headChange.author" class="flex-shrink-0" />
+      <Avatar size="5" :author="workspace.headChange.author" class="flex-shrink-0" />
       <span class="text-gray-900 text-sm font-medium">
         <span class="text-gray-900 text-sm font-medium">
           Based on
           <span class="text-gray-500 font-medium">
-            {{ data.workspace.headChange.title }}
+            {{ workspace.headChange.title }}
           </span>
         </span>
       </span>
@@ -29,6 +29,6 @@ import Avatar from '../../shared/Avatar.vue'
 
 export default {
   components: { Avatar },
-  props: ['data', 'codebaseSlug'],
+  props: ['workspace', 'codebaseSlug'],
 }
 </script>
--- web/src/organisms/WorkspaceDetails.vue
+++ web/src/organisms/WorkspaceDetails.vue
@@ -1,63 +1,181 @@
 <template>
-  <h2 class="sr-only">Details</h2>
-  <div class="space-y-5">
-    <Presence
-      :presence="data.workspace.presence"
-      :workspace="data.workspace"
-      :user="user"
-      class="hidden lg:flex"
-    />
-    <Comments :data="data" />
-    <UpdatedAt :data="data" />
-    <BasedOn :data="data" :codebase-slug="codebaseSlug" />
-    <Watching
-      v-if="isAuthorized && !isSelfOwnedWorkspace"
-      :user="user"
-      :watchers="data.workspace.watchers"
-      :workspace-id="data.workspace.id"
-    />
-    <StatusDetails :statuses="data.workspace.statuses" />
-    <GitHubPullRequest
-      :git-hub-integration="data?.workspace?.codebase?.gitHubIntegration"
-      :git-hub-pull-request="data?.workspace?.gitHubPullRequest"
-    />
-  </div>
-  <div class="mt-6 border-t border-gray-200 py-6 space-y-8">
-    <div>
-      <h2 class="text-sm font-medium text-gray-500">Author</h2>
-      <ul role="list" class="mt-3 space-y-3">
-        <li class="flex justify-start">
-          <a href="#" class="flex items-center space-x-3">
-            <div class="flex-shrink-0">
-              <Avatar :author="data.workspace.author" size="5" />
-            </div>
-            <div class="text-sm font-medium text-gray-900">
-              {{ data.workspace.author.name }}
-            </div>
-          </a>
-        </li>
-      </ul>
+  <div class="flex flex-col gap-4">
+    <div class="grid grid-flow-row-dense grid-cols-2 gap-4 xl:grid-cols-1">
+      <Presence :workspace="workspace" :user="user" class="hidden xl:flex" />
+      <Comments :workspace="workspace" />
+      <UpdatedAt :workspace="workspace" />
+      <BasedOn :workspace="workspace" :codebase-slug="codebaseSlug" />
+      <Watching
+        v-if="isAuthorized && !isSelfOwnedWorkspace"
+        :user="user"
+        :watchers="workspace.watchers"
+        :workspace-id="workspace.id"
+      />
+      <StatusDetails :statuses="workspace.statuses" />
+      <GitHubPullRequest
+        :git-hub-integration="workspace.codebase?.gitHubIntegration"
+        :git-hub-pull-request="workspace.gitHubPullRequest"
+      />
     </div>
-    <WorkspaceApproval
-      v-if="showApproval"
-      :reviews="data.workspace.reviews"
-      :workspace="data.workspace"
-      :codebase-id="data.workspace.codebase.id"
-      :user="user"
-      :members="data.workspace.codebase.members"
-    />
-  </div>
 
-  <WorkspaceActivitySidebar
-    v-if="showActivity"
-    class="mt-6"
-    :workspace="data.workspace"
-    :codebase-slug="codebaseSlug"
-    :user="user"
-  />
+    <div class="flex gap-6 border-t border-gray-200 py-6 justify-evenly xl:flex-col">
+      <div class="flex flex-col flex-1">
+        <h2 class="text-sm font-medium text-gray-500">Author</h2>
+        <ul role="list" class="mt-3 space-y-3">
+          <li class="flex justify-start">
+            <a href="#" class="flex items-center space-x-3">
+              <div class="flex-shrink-0">
+                <Avatar :author="workspace.author" size="5" />
+              </div>
+              <div class="text-sm font-medium text-gray-900">
+                {{ workspace.author.name }}
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <WorkspaceApproval
+        class="flex-1"
+        v-if="showApproval"
+        :reviews="workspace.reviews"
+        :workspace="workspace"
+        :codebase-id="workspace.codebase.id"
+        :user="user"
+        :members="workspace.codebase.members"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-export default defineComponent({})
+import { defineComponent, PropType } from 'vue'
+import { gql } from '@urql/vue'
+
+import Presence, {
+  WORKSPACE_FRAGMENT as WORKSPACE_PRESENSE_FRAGMENT,
+} from '../components/workspace/Presence.vue'
+import Comments, {
+  WORKSPACE_FRAGMENT as COMMENTS_WORKSPACE_FRAGMENT,
+} from '../components/workspace/details/Comments.vue'
+import UpdatedAt, {
+  WORKSPACE_FRAGMENT as UPDATED_WORKSPACE_FRAGMENT,
+} from '../components/workspace/details/UpdatedAt.vue'
+import BasedOn from '../components/workspace/details/BasedOn.vue'
+import Watching, { WORKSPACE_WATCHER_FRAGMENT } from '../components/workspace/details/Watching.vue'
+import StatusDetails from '../components/statuses/StatusDetails.vue'
+import GitHubPullRequest, {
+  CODEBASE_GITHUB_INTEGRATION_FRAGMENT,
+  GITHUB_PULL_REQUEST_FRAGMENT,
+} from '../components/workspace/details/GitHubPullRequest.vue'
+import Avatar from '../components/shared/Avatar.vue'
+import { AUTHOR } from '../components/shared/AvatarHelper'
+import WorkspaceApproval from '../components/workspace/WorkspaceApproval.vue'
+
+import { Slug } from '../slug'
+import { WorkspaceDetails_WorkspaceFragment } from './__generated__/WorkspaceDetails'
+
+export const WORKSPACE_FRAGMENT = gql`
+  fragment WorkspaceDetails_Workspace on Workspace {
+    id
+    author {
+      ...Author
+    }
+    watchers {
+      ...WorkspaceWatcher
+    }
+    gitHubPullRequest @include(if: $isGitHubEnabled) {
+      ...GitHubPullRequest
+    }
+    headChange {
+      id
+      title
+      author {
+        ...Author
+      }
+    }
+    codebase {
+      id
+      name
+      shortID
+      gitHubIntegration @include(if: $isGitHubEnabled) {
+        ...CodebaseGitHubIntegration
+      }
+      members {
+        ...Author
+      }
+    }
+    reviews {
+      id
+      grade
+      createdAt
+      isReplaced
+      dismissedAt
+      author {
+        id
+        name
+        avatarUrl
+      }
+    }
+    suggestion {
+      id
+    }
+    ...Presence_Workspace
+    ...CommentsCount_Workspace
+    ...Updated_Workspace
+  }
+  ${CODEBASE_GITHUB_INTEGRATION_FRAGMENT}
+  ${GITHUB_PULL_REQUEST_FRAGMENT}
+  ${WORKSPACE_WATCHER_FRAGMENT}
+  ${WORKSPACE_PRESENSE_FRAGMENT}
+  ${COMMENTS_WORKSPACE_FRAGMENT}
+  ${UPDATED_WORKSPACE_FRAGMENT}
+  ${AUTHOR}
+`
+
+type Author = WorkspaceDetails_WorkspaceFragment['author']
+
+export default defineComponent({
+  components: {
+    Presence,
+    Comments,
+    UpdatedAt,
+    BasedOn,
+    Watching,
+    StatusDetails,
+    GitHubPullRequest,
+    Avatar,
+    WorkspaceApproval,
+  },
+  props: {
+    workspace: {
+      type: Object as PropType<WorkspaceDetails_WorkspaceFragment>,
+      required: true,
+    },
+    user: {
+      type: Object as PropType<Author>,
+    },
+  },
+  computed: {
+    codebaseSlug() {
+      return Slug(this.workspace.codebase.name, this.workspace.codebase.shortID)
+    },
+    isAuthenticated() {
+      return !!this.user
+    },
+    isAuthorized() {
+      const isMember = this.workspace.codebase.members.some(({ id }) => id === this.user?.id)
+      return this.isAuthenticated && isMember
+    },
+    isSelfOwnedWorkspace() {
+      return this.workspace && this.workspace.author.id === this.user?.id
+    },
+
+    isSuggesting() {
+      return !!this.workspace.suggestion
+    },
+    showApproval() {
+      return !this.isSuggesting
+    },
+  },
+})
 </script>
