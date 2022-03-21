79175fe781152648e1840b2c7292663c23014d4a
Nikita Galaiko <nikita@galaiko.rocks>
2022-03-21 15:07:02 +0000

    web/workspace: add comments count component fragment
    Created with Sturdy

--- web/src/components/workspace/details/Comments.vue
+++ web/src/components/workspace/details/Comments.vue
@@ -1,21 +1,39 @@
 <template>
   <div class="flex items-center space-x-2">
     <ChatAltIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-    <span v-if="data.workspace.comments.length === 0" class="text-gray-900 text-sm font-medium">
-      No comments
-    </span>
+    <span v-if="commentsCount === 0" class="text-gray-900 text-sm font-medium">No comments</span>
     <span v-else class="text-gray-900 text-sm font-medium">
-      {{ data.workspace.comments.length }}
-      {{ data.workspace.comments.length === 1 ? 'comment' : 'comments' }}
+      {{ commentsCount }}
+      {{ commentsCount === 1 ? 'comment' : 'comments' }}
     </span>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { ChatAltIcon } from '@heroicons/vue/solid'
+import { gql } from '@urql/vue'
+import { CommentsCount_WorkspaceFragment } from './__generated__/Comments'
+import { defineComponent, PropType } from 'vue'
 
-export default {
+export const WORKSPACE_FRAGMENT = gql`
+  fragment CommentsCount_Workspace on Workspace {
+    id
+    commentsCount
+  }
+`
+
+export default defineComponent({
   components: { ChatAltIcon },
-  props: ['data'],
-}
+  props: {
+    workspace: {
+      type: Object as PropType<CommentsCount_WorkspaceFragment>,
+      required: true,
+    },
+  },
+  computed: {
+    commentsCount() {
+      return this.workspace.commentsCount
+    },
+  },
+})
 </script>
