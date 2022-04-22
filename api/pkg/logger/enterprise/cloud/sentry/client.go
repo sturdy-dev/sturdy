221742f8cd974fc7b127c84ce29787cf6708dbc6
Nikita Galaiko <nikita@galaiko.rocks>
2022-04-22 06:45:57 +0000

    api/logger: do not context cancelled to sentry
    Created with Sturdy

--- api/pkg/logger/enterprise/cloud/sentry/client.go
+++ api/pkg/logger/enterprise/cloud/sentry/client.go
@@ -1,14 +1,18 @@
 package sentry
 
 import (
+	"context"
+
 	"getsturdy.com/api/pkg/version"
+
 	"github.com/getsentry/sentry-go"
 )
 
 func NewClient() (*sentry.Client, error) {
 	return sentry.NewClient(sentry.ClientOptions{
-		Dsn:        "https://7ca135fcbbbc4fa3bf816695f743c98f@o952367.ingest.sentry.io/6177866",
-		ServerName: version.Type.String(),
-		Release:    version.Version,
+		Dsn:          "https://7ca135fcbbbc4fa3bf816695f743c98f@o952367.ingest.sentry.io/6177866",
+		ServerName:   version.Type.String(),
+		Release:      version.Version,
+		IgnoreErrors: []string{context.Canceled.Error()},
 	})
 }
