5810f3f4e28086515a073c3cf81ba2b4f991dda3
Gustav Westling <gustav@getsturdy.com>
2022-04-22 07:33:08 +0000

    web/Handbook: add Contributions page for how to handle external contributions
    Created with Sturdy

---
+++ web/src/pages/docs/handbook/Contributions.vue
@@ -0,0 +1,43 @@
+<template>
+  <DocumentationWithTableOfContents>
+    <template #sidebar>
+      <HandbookSidebar />
+    </template>
+
+    <template #default>
+      <div class="prose">
+        <h1>External Contributions</h1>
+
+        <p>
+          Sturdy accepts (and welcomes!) contributions to our code from external parties. We're
+          currently requiring all contributors to sign a Contributor License Agreement (CLA) before
+          they can contribute.
+        </p>
+
+        <ol>
+          <li>
+            The contributor (or their employer) needs to sign the
+            <a href="https://forms.gle/u8tfb8LyNtLNitQ27">Contributor License Agreement (CLA)</a>
+          </li>
+          <li>
+            <em>Sturdy Employee:</em>
+            <a
+              href="https://docs.google.com/spreadsheets/d/1YyYFxEYo8dovmvrVMUHx_gC0EyLga9UztrteVzRmwrY/edit?usp=sharing"
+              >Verify</a
+            >
+            that the contributor has filled in the form
+          </li>
+          <li>
+            <em>Sturdy Employee:</em> Add the contributors GitHub username to
+            <a href="https://getsturdy.com/clabot-config-AySxptY">the .clabot configuration</a>
+          </li>
+        </ol>
+      </div>
+    </template>
+  </DocumentationWithTableOfContents>
+</template>
+
+<script lang="ts" setup>
+import DocumentationWithTableOfContents from '../../../layouts/DocumentationWithTableOfContents.vue'
+import HandbookSidebar from '../../../organisms/docs/HandbookSidebar.vue'
+</script>
