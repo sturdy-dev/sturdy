57d7dc0d3c6e71889fc750d413fb087f342c5248
Nikita Galaiko <nikita@galaiko.rocks>
2022-04-25 08:55:51 +0000

    api/cmd: use zap logger
    Created with Sturdy

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"log"
-	"os"
 
 	"getsturdy.com/api/pkg/api"
 	apiModule "getsturdy.com/api/pkg/api/module"
@@ -15,6 +12,7 @@ import (
 	"getsturdy.com/api/pkg/di"
 
 	"github.com/jmoiron/sqlx"
+	"go.uber.org/zap"
 )
 
 func main() {
@@ -29,23 +27,24 @@ func main() {
 		ctx       context.Context
 		dm        *datamigrations.Service
 		sqlxdb    *sqlx.DB
+		logger    *zap.Logger
 	)
-	if err := di.Init(app).To(&apiServer, &ctx, &dm, &sqlxdb); err != nil {
-		fmt.Printf("%+v\n", err)
-		os.Exit(1)
+
+	if err := di.Init(app).To(&apiServer, &ctx, &dm, &sqlxdb, &logger); err != nil {
+		logger.Fatal("failed to init", zap.Error(err))
 	}
 
 	if err := dm.Run(ctx); err != nil {
-		log.Fatalf("failed to run data migrations: %s", err)
+		logger.Fatal("failed to run data migrations", zap.Error(err))
 	}
 
 	if err := db.MigrateUP(sqlxdb.DB); err != nil {
-		log.Fatalf("failed to migrate up: %s", err)
+		logger.Fatal("failed to migrate up", zap.Error(err))
 	}
 
 	banner.PrintBanner()
 
 	if err := apiServer.Start(ctx); err != nil {
-		log.Fatalf("faild to start server: %s", err)
+		logger.Fatal("failed to start server", zap.Error(err))
 	}
 }
