8d3e05f6e4394cfd7f4cc3e126c39d97735533eb
Nikita Galaiko <nikita@galaiko.rocks>
2022-04-25 07:33:44 +0000

    api/datamigrations: setup data migrations run only on the sturdy codebase to start with
    Created with Sturdy

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -10,20 +10,37 @@ import (
 	apiModule "getsturdy.com/api/pkg/api/module"
 	"getsturdy.com/api/pkg/banner"
 	xcontext "getsturdy.com/api/pkg/context"
+	"getsturdy.com/api/pkg/datamigrations"
+	"getsturdy.com/api/pkg/db"
 	"getsturdy.com/api/pkg/di"
+
+	"github.com/jmoiron/sqlx"
 )
 
 func main() {
 	app := func(c *di.Container) {
 		c.Import(apiModule.Module)
 		c.Import(xcontext.Module)
+		c.Import(datamigrations.Module)
 	}
 
-	var apiServer api.Starter
-	var ctx context.Context
-	if err := di.Init(app).To(&apiServer, &ctx); err != nil {
+	var (
+		apiServer api.Starter
+		ctx       context.Context
+		dm        *datamigrations.Service
+		sqlxdb    *sqlx.DB
+	)
+	if err := di.Init(app).To(&apiServer, &ctx, &dm, &sqlxdb); err != nil {
 		fmt.Printf("%+v\n", err)
 		os.Exit(1)
+	}
+
+	if err := dm.Run(ctx); err != nil {
+		log.Fatalf("failed to run data migrations: %s", err)
+	}
+
+	if err := db.MigrateUP(sqlxdb.DB); err != nil {
+		log.Fatalf("failed to migrate up: %s", err)
 	}
 
 	banner.PrintBanner()
