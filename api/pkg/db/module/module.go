fcc23e0ab7e9a5131a482e19bba28154b230011c
Gustav Westling <gustav@getsturdy.com>
2022-04-26 08:46:53 +0000

    api/pkg/db: move forgotten migrations
    Created with Sturdy

--- api/pkg/db/module/module.go
+++ api/pkg/db/module/module.go
@@ -14,14 +14,4 @@ func Module(c *di.Container) {
 
 func TestModule(c *di.Container) {
 	c.Import(configuration.TestModule)
-	// c.Register(func(config *configuration_db.Configuration, dm *datamigrations.Service) (*sqlx.DB, error) {
-	// 	db, err := db.FromConfiguration(config)
-	// 	if err != nil {
-	// 		return nil, fmt.Errorf("could not connect to database: %w", err)
-	// 	}
-	// 	if err := db.MigrateUP(db.DB, dm); err != nil {
-	// 		return nil, fmt.Errorf("could not migrate up: %w", err)
-	// 	}
-	// 	return db, err
-	// })
 }
