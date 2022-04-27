95260cce4b360071847a05463a1a3192b01d4136
Nikita Galaiko <nikita@galaiko.rocks>
2022-04-27 12:54:57 +0000

    api/pkg/db: share missing packages
    Created with Sturdy

---
+++ api/pkg/db/migrate/data/module.go
@@ -0,0 +1,17 @@
+package data
+
+import (
+	service_changes "getsturdy.com/api/pkg/changes/service"
+	service_codebases "getsturdy.com/api/pkg/codebases/service"
+	"getsturdy.com/api/pkg/di"
+	"getsturdy.com/api/pkg/logger"
+	"getsturdy.com/api/pkg/review/db"
+)
+
+func Module(c *di.Container) {
+	c.Import(db.Module)
+	c.Import(service_changes.Module)
+	c.Import(service_codebases.Module)
+	c.Import(logger.Module)
+	c.Register(NewService)
+}
