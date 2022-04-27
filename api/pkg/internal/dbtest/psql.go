154e4d3af5798e5ea7acf690a8ee7ce9c9e9cfa8
Nikita Galaiko <nikita@galaiko.rocks>
2022-04-27 12:51:39 +0000

    api/db: fix e2e tests
    Created with Sturdy

--- api/pkg/internal/dbtest/psql.go
+++ api/pkg/internal/dbtest/psql.go
@@ -1,17 +1,33 @@
 package dbtest
 
 import (
+	"testing"
 	"time"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/assert"
 
 	"getsturdy.com/api/pkg/db"
 	"getsturdy.com/api/pkg/db/migrate/schema"
 	"getsturdy.com/api/pkg/internal/sturdytest"
 )
 
-func DB() (*sqlx.DB, error) {
-	sqldb, err := db.SetupWithTimeout(sturdytest.PsqlDbSourceForTesting(), time.Millisecond)
+func DB(t *testing.T) *sqlx.DB {
+	sqldb, err := getDB()
+	assert.NoError(t, err)
+	return sqldb
+}
+
+func MustGetDB() *sqlx.DB {
+	db, err := getDB()
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func getDB() (*sqlx.DB, error) {
+	sqldb, err := db.SetupWithTimeout(sturdytest.PsqlDbSourceForTesting(), time.Second*5)
 	if err != nil {
 		return nil, err
 	}
